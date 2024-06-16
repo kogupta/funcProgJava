9cdd080df4f64f9a81673e3ab21e2400538411aa
kogupta <kohinoor.gupta@gmail.com>
2024-06-16 17:20:01 +0530

    init

--- src/main/java/fpJava/ch3/Emails.java
+++ src/main/java/fpJava/ch3/Emails.java
@@ -1,15 +1,15 @@
 package fpJava.ch3;
 
-import fpJava.ch3.Main.Result.Failure;
-import fpJava.ch3.Main.Result.Success;
+import fpJava.ch3.Emails.Result.Failure;
+import fpJava.ch3.Emails.Result.Success;
 
 import java.util.regex.Pattern;
 
-public final class Main {
+public final class Emails {
     private static final Pattern emailPattern =
             Pattern.compile("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,5}$");
 
-    private Main() {}
+    private Emails() {}
 
     public static void main(String[] args) {
         emailHandler("");
@@ -19,11 +19,7 @@ public final class Main {
     }
 
     private static void emailHandler(String email) {
-        Result<String> result = validateEmail(email);
-        switch (result) {
-            case Failure(var error) -> System.err.println("Error: " + error);
-            case Success(var s) -> System.out.println("Email sent to: " + s);
-        }
+        toExec(validateEmail(email)).run();
     }
 
     private static Result<String> validateEmail(String email) {
@@ -34,22 +30,36 @@ public final class Main {
                 Result.failure("email is not valid");
     }
 
+    private static Executable toExec(Result<String> result) {
+        return switch (result) {
+            case Failure(var error) -> () -> System.err.println("Error: " + error);
+            case Success(var s) -> () -> System.out.println("Email sent to: " + s);
+        };
+    }
+
     private static void assertThat(boolean predicate, String msg) {
         if (!predicate)
             throw new AssertionError(msg);
     }
 
     sealed interface Result<T> {
-        static <T> Result<T> success(T value) {
-            return new Success<>(value);
-        }
+        static <T> Result<T> success(T value) {return new Success<>(value);}
 
-        static <T> Result<T> failure(String error) {
-            return new Failure<>(error);
-        }
+        static <T> Result<T> failure(String error) {return new Failure<>(error);}
+
+        T value();
 
         record Success<T>(T value) implements Result<T> {}
 
-        record Failure<T>(String error) implements Result<T> {}
+        record Failure<T>(String error) implements Result<T> {
+            @Override
+            public T value() {
+                throw new RuntimeException("No value present");
+            }
+        }
+    }
+
+    public interface Executable {
+        void run();
     }
 }
