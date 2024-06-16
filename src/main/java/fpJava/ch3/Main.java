3943f459ae37572325715498c87618762f33c5dc
kogupta <kohinoor.gupta@gmail.com>
2024-06-16 14:12:45 +0530

    init

--- src/main/java/fpJava/ch3/Main.java
+++ src/main/java/fpJava/ch3/Main.java
@@ -1,20 +1,55 @@
 package fpJava.ch3;
 
+import fpJava.ch3.Main.Result.Failure;
+import fpJava.ch3.Main.Result.Success;
+
+import java.util.regex.Pattern;
+
 public final class Main {
-    private Main() {
-    }
+    private static final Pattern emailPattern =
+            Pattern.compile("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,5}$");
+
+    private Main() {}
 
     public static void main(String[] args) {
-        _assertionStatus();
+        emailHandler("");
+        emailHandler(null);
+        emailHandler("john.doe@acme.org");
+        emailHandler("this.is@my.email");
     }
 
-    private static void _assertionStatus() {
-        String status = Main.class.desiredAssertionStatus() ? "enabled" : "disabled";
-        System.out.println("assertions: " + status);
+    private static void emailHandler(String email) {
+        Result<String> result = validateEmail(email);
+        switch (result) {
+            case Failure(var error) -> System.err.println("Error: " + error);
+            case Success(var s) -> System.out.println("Email sent to: " + s);
+        }
+    }
+
+    private static Result<String> validateEmail(String email) {
+        if (email == null || email.isEmpty())
+            return Result.failure("email must not be null/empty");
+        return emailPattern.matcher(email).matches() ?
+                Result.success(email) :
+                Result.failure("email is not valid");
     }
 
     private static void assertThat(boolean predicate, String msg) {
         if (!predicate)
             throw new AssertionError(msg);
+    }
+
+    sealed interface Result<T> {
+        static <T> Result<T> success(T value) {
+            return new Success<>(value);
+        }
+
+        static <T> Result<T> failure(String error) {
+            return new Failure<>(error);
+        }
+
+        record Success<T>(T value) implements Result<T> {}
+
+        record Failure<T>(String error) implements Result<T> {}
     }
 }
