b041b97baa44386d4df0532e897f7f1d04947fd9
kogupta <kohinoor.gupta@gmail.com>
2024-06-17 22:59:53 +0530

    chapter 4: chapter 4: chapter 4: ...

--- src/main/java/fpJava/ch4/Fib.java
+++ src/main/java/fpJava/ch4/Fib.java
@@ -1,5 +1,9 @@
 package fpJava.ch4;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
 public final class Fib {
     private Fib() {}
 
@@ -17,8 +21,7 @@ public final class Fib {
     }
 
     public static long recur(int n) {
-        if (n < 3) return 1;
-        return _recur(n - 2, 1, 1);
+        return n < 3 ? 1 : _recur(n - 2, 1, 1);
     }
 
     private static long _recur(int n, long prev, long acc) {
@@ -41,6 +44,36 @@ public final class Fib {
                 TailCall.suspend(() -> _heapifyRecur(n - 1, acc, acc + prev));
     }
 
+    private static <T> List<T> iterate(T seed, Function<T, T> generator, int count) {
+        List<T> result = new ArrayList<>(count);
+        T t = seed;
+        for (int i = 0; i < count; i++) {
+            result.add(t);
+            t = generator.apply(t);
+        }
+
+        return result;
+    }
+
+    private static <T> String mkString(List<T> xs, String separator) {
+        return _mkString(new StringBuilder(), xs, separator);
+    }
+
+    private static <T> String _mkString(StringBuilder acc, List<T> xs, String separator) {
+        if (xs.isEmpty()) return acc.toString();
+        acc.append(xs.getFirst());
+        if (xs.size() > 1)
+            acc.append(separator);
+        return _mkString(acc, xs.subList(1, xs.size()), separator);
+    }
+
+    public static String coRecursive(int n) {
+        Pair seed = new Pair(1, 1);
+        List<Pair> pairs = iterate(seed, Pair::next, n);
+        List<Long> fibs = pairs.stream().map(Pair::first).toList();
+        return mkString(fibs, ", ");
+    }
+
     public static void main(String[] args) {
         for (int i = 1; i < 10; i++) {
             System.out.println(loop(i) + "  " + recur(i) + "  " + heapifyRecur(i).eval());
@@ -50,6 +83,14 @@ public final class Fib {
             long expected = loop(i);
             assertThat(recur(i) == expected, "fib mismatch at: " + i);
             assertThat(heapifyRecur(i).eval() == expected, "fib mismatch at: " + i);
+        }
+
+        assertThat(coRecursive(10).equals("1, 1, 2, 3, 5, 8, 13, 21, 34, 55"), "...");
+    }
+
+    record Pair(long first, long second) {
+        Pair next() {
+            return new Pair(second, first + second);
         }
     }
 
